3ddb8fd5ad95809f6050fffea118cbf14113ada4
MrManos <jacobhands77@gmail.com>
2023-11-23 21:14:57 -0600

    i started something

---
+++ amazon_delivery.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+#include <algorithm> 
+#include <cmath> 
+
+class:: address{
+public:
+
+int i, j; 
+
+std::string Last_possible_delivery_date
+
+address(int &i, int &j, std::string Last_possible_delivery_date){
+
+    
+}
+}
