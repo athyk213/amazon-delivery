3f4a116a8b15ff682a1ff1430efa7a7221109b19
Jacob Hands <jacobhands77@gmail.com>
2023-11-24 16:09:54 -0600

    build and updated a bit

--- amazon_delivery.cpp
+++ amazon_delivery.cpp
@@ -10,7 +10,7 @@ public:
     // Constructor
     Address(int i, int j, std::string Last_possible_delivery_date) : i(i), j(j), Last_possible_delivery_date(Last_possible_delivery_date) {}
 
-    // Distance calculation method
+    // manhattan_distance and straight line mehtod
     double distance(const Address& additional_addresses, bool manhattan_distance = true) const {
         if (manhattan_distance) {
             return std::abs(i - additional_addresses.i) + std::abs(j - additional_addresses.j);
@@ -23,14 +23,20 @@ public:
 int main() {
 
     // test code from chatgpt
-    Address address1(1, 2, "2023-11-30");
-    Address address2(3, 5, "2023-11-25");
+    Address baseaddress(0,0, "2023-1-1");
+    Address address1(2, 4, "2023-11-30");
+    Address address2(6, 7, "2023-11-25");
 
-    double manhattan_distance = address1.distance(address2);
-    double straight_line_distance = address1.distance(address2, false);
+    double manhattan_distance = baseaddress.distance(address1);
+    double straight_line_distance = baseaddress.distance(address1, false);
+
+    double manhattan_distance1 = baseaddress.distance(address2);
+    double straight_line_distance1 = baseaddress.distance(address2, false);
 
     std::cout << "Manhattan Distance: " << manhattan_distance << std::endl;
     std::cout << "Straight-Line Distance: " << straight_line_distance << std::endl;
+    std::cout << "Manhattan Distance: " << manhattan_distance1 << std::endl;
+    std::cout << "Straight-Line Distance: " << straight_line_distance1 << std::endl;
 
     return 0;
 };
