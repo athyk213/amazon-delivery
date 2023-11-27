dc0eed45e56f506bd640dc89c7d8d2f27b710e8b
BuildTools <unconfigured@null.spigotmc.org>
2023-11-27 11:57:55 -0600

    Address list

--- amazon_delivery.cpp
+++ amazon_delivery.cpp
@@ -4,12 +4,20 @@
 #include <vector>
 
 class Address {
-public:
+private:
     int i, j;
     std::string Last_possible_delivery_date;
-
+public:
     // Constructor
     Address(int i, int j, std::string Last_possible_delivery_date = "No_Date") : i(i), j(j), Last_possible_delivery_date(Last_possible_delivery_date) {}
+    Address(){}
+    double get_i() const {
+        return i;
+    }
+
+    double get_j() const {
+        return j;
+    }
 
     // manhattan_distance and straight line mehtod
     double distance(const Address& additional_addresses, bool manhattan_distance = true) const {
@@ -19,6 +27,11 @@ public:
             return std::sqrt(std::pow(i - additional_addresses.i, 2) + std::pow(j - additional_addresses.j, 2));
         }
     }
+
+    // check if addresses are equal
+    bool operator==(const Address& other) const {
+        return i == other.get_i() && j == other.get_j();
+    }
 };
 
 class AddressList{
@@ -26,30 +39,68 @@ class AddressList{
         std::vector<Address> address_list;
     public:
         
-        void add_address(){};
+        void add_address(Address address){
+            // Ensure the address is not already in the list
+            for (const auto& addr : address_list) {
+                if (addr == address) {
+                    return;
+                }
+            }
 
-        double length(){};
+            // add address to address list
+            address_list.push_back(address);
+        };
 
-        Address index_closest_to(){};
+        // Calculate the total distance to visit all addresses in order
+        double length(){
+            double total_distance = 0.0;
+            for (int i = 0; i < address_list.size() - 1; ++i) {
+                total_distance += address_list[i].distance(address_list[i + 1]);
+            }
+            return total_distance;
+        };
+
+        // Find the index of the address in the list closest to the target_address
+        Address index_closest_to(Address address) {
+            double min_distance = INFINITY;
+            Address closest_address;
+
+            for (int i = 0; i < address_list.size(); ++i) {
+                double current_distance = address.distance(address_list[i]);
+                if (current_distance < min_distance) {
+                    min_distance = current_distance;
+                    closest_address = address_list[i];
+                }
+            }
+
+            return closest_address;
+        }
 };
 
 int main() {
+    // Create addresses
+    Address address1(1, 2, "2023-12-01");
+    Address address2(3, 4, "2023-12-02");
+    Address address3(5, 6, "2023-12-03");
+    Address address4(7, 8, "2023-12-04");
 
-    // test code from chatgpt
-    Address baseaddress(0,0, "2023-1-1");
-    Address address1(2, 4, "2023-11-30");
-    Address address2(6, 7, "2023-11-25");
+    // Create an AddressList
+    AddressList addressList;
 
-    double manhattan_distance = baseaddress.distance(address1);
-    double straight_line_distance = baseaddress.distance(address1, false);
+    // Add addresses to the list
+    addressList.add_address(address1);
+    addressList.add_address(address2);
+    addressList.add_address(address3);
+    addressList.add_address(address4);
 
-    double manhattan_distance1 = baseaddress.distance(address2);
-    double straight_line_distance1 = baseaddress.distance(address2, false);
+    // Calculate and print the total distance to visit all addresses in order
+    std::cout << "Total distance: " << addressList.length() << std::endl;
 
-    std::cout << "Manhattan Distance: " << manhattan_distance << std::endl;
-    std::cout << "Straight-Line Distance: " << straight_line_distance << std::endl;
-    std::cout << "Manhattan Distance: " << manhattan_distance1 << std::endl;
-    std::cout << "Straight-Line Distance: " << straight_line_distance1 << std::endl;
+    // Find the address closest to a target address
+    Address targetAddress(10, 10);
+    Address closestAddress = addressList.index_closest_to(targetAddress);
+
+    std::cout << "Closest address to target: (" << closestAddress.get_i() << ", " << closestAddress.get_j() << ")" << std::endl;
 
     return 0;
-};
+}
